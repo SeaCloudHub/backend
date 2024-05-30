a79bab736de973c16d7570be4eed6c4c7fc17441
phihungtf <phihungtf@gmail.com>
2024-05-30 22:16:46 +0700

    Edit validation for avatar URL in EditIdentityRequest

--- adapters/httpserver/model/admin.go
+++ adapters/httpserver/model/admin.go
@@ -166,7 +166,7 @@ type EditIdentityRequest struct {
 	IdentityID string `param:"identity_id" validate:"required,uuid" swaggerignore:"true"`
 	FirstName  string `json:"first_name" validate:"omitempty,max=50"`
 	LastName   string `json:"last_name" validate:"omitempty,max=50"`
-	AvatarURL  string `json:"avatar_url" validate:"omitempty,url"`
+	AvatarURL  string `json:"avatar_url" validate:"omitempty,url|filepath"`
 } // @name model.EditIdentityRequest
 
 func (r *EditIdentityRequest) Validate() error {
